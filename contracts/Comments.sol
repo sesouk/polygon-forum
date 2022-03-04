b87405918afff69164e3227de7c089dae7f63f29
Kevin Tran <sesouk@gmail.com>
2022-03-03 19:36:29 -0700

    started on ui

--- contracts/Comments.sol
+++ contracts/Comments.sol
@@ -12,9 +12,25 @@ contract Comments {
     uint256 created_at;
   }
 
+  uint32 private idCounter;
+  mapping(string => Comment[]) private commentsByTopic;
+
   event CommentAdded(Comment comment);
 
-  function getcomments(string calldata topic) public view returns(Comment[] memory){}
+  function getComments(string calldata topic) public view returns(Comment[] memory){
+    return commentsByTopic[topic];
+  }
 
-  function addComment(string calldata tpic, string calldata message) public {}
+  function addComment(string calldata topic, string calldata message) public {
+    Comment memory comment = Comment({
+      id: idCounter,
+      topic: topic,
+      creator_address: msg.sender,
+      message: message,
+      created_at: block.timestamp
+    });
+    commentsByTopic[topic].push(comment);
+    idCounter++;
+    emit CommentAdded(comment);
+  }
 }
