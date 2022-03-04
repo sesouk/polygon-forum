46d99fac292c41ae2c17edcc1690c3d2af1f0d5e
Kevin Tran <sesouk@gmail.com>
2022-03-03 19:53:40 -0700

    adding hooks

---
+++ hooks/useComments.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "react-query";
+import useCommentsContract from "./useCommentsContracts";
+
+interface UseCommentsQuery {
+  topic: string;
+}
+
+const useComments = ({ topic }: UseCommentsQuery)  => {
+  const contract = useCommentsContract()
+
+  return useQuery(["comments", { topic, chainId: contract.chainId }], () => 
+    contract.getComments(topic)
+  )
+}
+
+export default useComments
